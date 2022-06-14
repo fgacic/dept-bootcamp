f9e3cab883237465b0dae17bcf402c249cf0c4d9
fgacic <alien538@gmail.com>
2022-06-14 19:30:18 +0200

    zad1 done

---
+++ zad1.js
@@ -0,0 +1,45 @@
+var compareNumbers = (a, b) => b - a;
+
+class Array {
+  constructor() {
+    (this.list = []), (this.sum = 0);
+  }
+  addValue(value) {
+    this.list.push(value);
+    this.calculateSum();
+  }
+  calculateSum() {
+    this.sum = this.list.reduce(
+      (previousValue, currentValue) => previousValue + currentValue,
+      0
+    );
+  }
+}
+
+//CHANGE THIS
+var n = 3;
+var masterArray = [8, 1, 5, 2, 4, 1, 9, 8];
+
+masterArray.sort(compareNumbers);
+var list = [];
+for (var i = 0; i < n; i++) {
+  list.push(new Array());
+}
+
+while (masterArray.length > 0) {
+  var shiftedValue = masterArray.shift(0);
+  //search for the lower/higher sum array to put in next positive/negative value
+  var nextArray = list.reduce((prev, curr) => {
+    if (shiftedValue > 0) return curr.sum < prev.sum ? curr : prev;
+    else return curr.sum > prev.sum ? curr : prev;
+  });
+  nextArray.addValue(shiftedValue);
+}
+
+list.forEach((array) => {
+  console.log(array.list + " sum: ", array.sum);
+});
+
+var a = list.reduce((prev, curr) => {
+  return curr.sum < prev.sum ? curr : prev;
+});
