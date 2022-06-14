5730a889bf9817f66e4d4f93145b511e09b79363
fgacic <alien538@gmail.com>
2022-06-14 20:07:32 +0200

    2done
    Signed-off-by: fgacic <alien538@gmail.com>

---
+++ zad2withComma.js
@@ -0,0 +1,19 @@
+//CHANGE THIS
+var pyramidHeight = 6;
+
+var k = pyramidHeight - 1;
+
+for (var i = 0; i < pyramidHeight; i++) {
+  for (var j = 0; j < k; j++) {
+    process.stdout.write("  ");
+  }
+  for (var j = 0; j < i * 2 + 1; j++) {
+    if (i != 0 && j != 0) process.stdout.write("," + (pyramidHeight - i));
+    else process.stdout.write("" + (pyramidHeight - i));
+  }
+  for (var j = 0; j < k; j++) {
+    process.stdout.write(" ");
+  }
+  k--;
+  console.log();
+}
